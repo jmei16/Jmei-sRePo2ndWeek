b8aefd5e7b2f802fd6123b17e1e746251c6d1a9c
jmei16 <67976097+jmei16@users.noreply.github.com>
2020-07-13 20:34:41 -0500

    Add files via upload

---
+++ Jia Ming_M_Calculator_Exercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//Top section
+let valueOne = 1
+let valueTwo = 2
+
+//Mid section
+let addition = valueOne + valueTwo
+let subtraction = valueOne - valueTwo
+let multiplication = valueOne * valueTwo
+let division = valueOne / valueTwo
+
+//end section
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
